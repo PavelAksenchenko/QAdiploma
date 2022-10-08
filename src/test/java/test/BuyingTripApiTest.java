9af21f37be10908ceea5a70cee8a51830bec4b1c
PavelAksenchenko <angelracing@yandex.ru>
2022-10-08 14:18:33 +0300

    Add Api test
    Signed-off-by: PavelAksenchenko <angelracing@yandex.ru>

--- src/test/java/test/BuyingTripApiTest.java
+++ src/test/java/test/BuyingTripApiTest.java
@@ -5,9 +5,13 @@ import data.ApiUtils;
 import data.Card;
 import data.DataGenerator;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class BuyingTripApiTest {
@@ -36,5 +40,19 @@ public class BuyingTripApiTest {
     void shouldNotSendCreditRequestWithIncorrectName() {
         int statusCode = ApiUtils.getRequestStatusCode(invalidHolderCard, "/api/v1/credit");
         assertNotEquals(200, statusCode);
+    }
+
+    @Test
+    @DisplayName("Должен провести оплату за услуги с валидной карты")
+    void shoulldCheckValidBuyerPayment() {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getValidCard(), "/api/v1/pay");
+        assertEquals(200, statusCode);
+    }
+
+    @Test
+    @DisplayName("Должен провести оплату при покупке услуги в кредит")
+    void shouldCheckValidBuyerCreditPayment() {
+        int statusCode = ApiUtils.getRequestStatusCode(DataGenerator.getValidCard(), "/api/v1/credit");
+        assertEquals(200, statusCode);
     }
 }
